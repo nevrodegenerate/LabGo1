673ff248f5ca19fc7e46db37dcfb701b4a46013b
TarnishedGhost <tarnishedphoenix@gmail.com>
2024-06-13 17:40:06 +0300

    Create server.go

---
+++ server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"time"
+)
+
+type TimeResponse struct {
+	CurrentTime string `json:"currentTime"`
+}
+
+func timeHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	currentTimeUTC := time.Now().UTC().Format(time.RFC3339)
+	timeResponse := TimeResponse{CurrentTime: currentTimeUTC}
+
+	responseWriter.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(responseWriter).Encode(timeResponse)
+}
+
+func main() {
+	http.HandleFunc("/time", timeHandler)
+	log.Fatal(http.ListenAndServe(":8795", nil))
+}
