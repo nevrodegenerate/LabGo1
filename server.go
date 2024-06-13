e2d751699feb9fc92288dae386aeed28cfc497c9
TarnishedGhost <tarnishedphoenix@gmail.com>
2024-06-13 17:46:20 +0300

    Create server.go

--- server.go
+++ server.go
@@ -7,18 +7,19 @@ import (
 	"time"
 )
 
-// TimeResponse представляет структуру ответа с текущим временем
 type TimeResponse struct {
 	CurrentTime string `json:"currentTime"`
 }
 
-// timeHandler обрабатывает HTTP-запросы и возвращает текущее время в формате JSON
 func timeHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	currentTimeUTC := time.Now().UTC().Format(time.RFC3339)
 	timeResponse := TimeResponse{CurrentTime: currentTimeUTC}
 
 	responseWriter.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(responseWriter).Encode(timeResponse)
+	err := json.NewEncoder(responseWriter).Encode(timeResponse)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
